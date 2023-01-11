52472dfaf68202c241c432eea3a810139e12ef58
Chelsea S <csnider91@live.com>
2023-01-11 09:41:38 -0500

    completed homework

--- scripts/main.js
+++ scripts/main.js
@@ -31,10 +31,32 @@
   // Show me how to calculate the average price of all items listed in USD. Please `console.log` the average
   // required
 
+  // * instructors method
+
+  const prices = items
+    .filter(function (item) {
+      return item.currency_code === "USD";
+    })
+    .map(function (item) {
+      return item.price;
+    });
+
+  //or
+
+  //  const pricesInUSD = item
+  //    .filter(function (item) {
+  //      return item.currency_code === "USD";
+  //    })
+  //    .map(function (item) {
+  //      return item.price;
+  //    });
+  // *
+
+  // * My method
+
   function averagePrice(itemsArr) {
     console.log("Average price: ");
-    let sum = 0;
-    let avg = 0;
+    var avg = 0;
 
     // loop through each item in the itemsArr
     for (let i = 0; i < itemsArr.length; i++) {
@@ -45,12 +67,69 @@
         sum += item.price;
       }
     }
+    //*
 
     // calculate the average by dividing the sum by the number of items
     avg = sum / itemsArr.length;
     console.log("sum", sum, "avg", avg.toFixed(2));
+
+    averagePrice(items);
+
+    //teachers method to average price'
+    // with reduce, it grabes the first item, object, from the array
+    const sum = prices.reduce(function (acc, i) {
+      return acc + i;
+    });
+
+    const averageOfAllUSItems = sum / prices.length;
+
+    // * Teacher method
+    // using filter to find item price under 14 and above 18 for items associated with USD
+    const cheapItems = items.filter(
+      function (item) {
+        return (
+          item.price >= 14 && item.price <= 18 && item.currecy_code == "USD"
+        );
+      }.forEach((item) => {
+        console.log(item.title);
+      })
+    );
+
+    // Using filter to find items associated with currency GBP
+    const itemsGBP = items.filter(function (item) {
+      return item.currency_code === "GBP";
+    });
+    //*
+    //*
+    // find items seperated by material. find material with property of "wood"
+    const itemsMadeOFWood = items
+      .filter(function (item) {
+        return item.materials.includes("wood");
+      })
+      .map(function (item) {
+        return item.title;
+      })
+      .forEach(function (title) {
+        console.log(`${title}\n`);
+      });
+    // find items that are using 8 or more materials. This checks the .length of the entire material array and counts the items in the array
+    const eightOrMoreMaterials = items
+      .filter(function (item) {
+        return item.materials.length >= 8;
+      })
+      .forEach(function (item) {
+        console.log(`${item.title}\n`);
+      });
+    item.materials.forEach(function (material) {
+      console.log(`${material}\n`);
+    });
   }
-  averagePrice(items);
+  // fiter for who made certain items
+  const madeBySeller = items.filter(function (item) {
+    return (item.who_made = "i_did");
+  });
+
+  console.log(`${madeBySeller.length} were made by their sellers`);
 
   // ••••••••••••
 })();
